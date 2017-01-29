6dd2f29101ef532366ab853083c1afd93b5eaae3
Californium251 <californium251@gmail.com>
2017-01-30 00:19:23 +0300

    Полностью переделал задание

--- js/form.js
+++ js/form.js
@@ -3,117 +3,93 @@
  */
 'use strict';
 
-var uploadForm = {
-  htmlNode: document.querySelector('#upload-select-image'),
-  uploadfile: function() {
-    return this.htmlNode.querySelector('#upload-file')
+//Объявляем переменные
+
+var uploadForm = document.querySelector('.upload-form');
+var uploadPhotoInput = document.getElementById('upload-file');
+var uploadOverlay = document.querySelector('.upload-overlay');
+var uploadFormCancel = uploadOverlay.querySelector('.upload-form-cancel');
+var filters = document.querySelectorAll('[name = upload-filter]');
+var photoPreview = document.querySelector('img.filter-image-preview');
+var scaleField = {
+  htmlNode: document.querySelector('fieldset.upload-resize-controls'),
+  buttons: function () {
+    return this.htmlNode.querySelectorAll('button.upload-resize-control');
+  },
+  valueWindow: function() {
+    return this.htmlNode.querySelector('.upload-resize-controls-value');
+  },
+  restrictions: {
+    min: 0,
+    max: 100,
+    step: 25
   }
 };
-var overlayForm = {
-  self: function() {
-    return this
-  },
-  htmlNode: document.querySelector('.upload-overlay'),
-  cancelBtn: function() {
-    return this.htmlNode.querySelector('.upload-form-cancel')
-  },
-  filters: {
-    field: document.querySelector('.upload-filter-controls'),
-  },
-  mainPhoto: function() {
-    return this.htmlNode.querySelector('.upload-form-preview');
-  },
-  setResize: function(self) {
-    var resizeObj = {};
-    resizeObj.htmlNode = self.htmlNode.querySelector('fieldset.upload-resize-controls');
-    resizeObj.buttons = self.htmlNode.querySelectorAll('button.upload-resize-control');
-    resizeObj.input = self.htmlNode.querySelector('input.upload-resize-control');
-    resizeObj.min = 0;
-    resizeObj.max = 100;
-    self.resize = resizeObj;
-  },
-  resize: {}
-};
 
-overlayForm.setResize(overlayForm.self());
-overlayForm.resize.input.value = '100%';
-
-function toNumber(string) {
-  return string.substring(0, string.length - 1)
-}
-
-function changeInputValue(resizeObj, opsType) {
-  var inputValue = toNumber(resizeObj.input.value);
-  if ((opsType === '+') && (+inputValue + +25 <= resizeObj.max)) {
-    inputValue = +inputValue + 25;
-    resizeObj.input.value = inputValue + '%';
+//функция, которая изменяет значение окошка с масштабом
+function changeScaleValue(valueField, opsType, min, max, step) {
+  if ((opsType === 'dec') && (parseInt(valueField.value) - 25 >= min)) {
+    valueField.value = (parseInt(valueField.value) - step) + '%';
   }
-  if ((opsType === '-') && (+inputValue - +25 >= resizeObj.min)) {
-    inputValue -= +25;
-    resizeObj.input.value = inputValue + '%';
+  if ((opsType === 'inc') && (parseInt(valueField.value) + 25 <= max)) {
+    valueField.value = (parseInt(valueField.value) + step) + '%';
   }
 }
 
-function changeViewedForm (form1, form2) {
-  form1.classList.toggle('invisible');
-  form2.classList.toggle('invisible');
+//функция, изменяющая масштаб фото
+function changeScale (photo, scaleVal) {
+  photo.style.transform = 'scale(' + parseInt(scaleVal) / 100 + ')';
 }
 
-function changeFilter(filterList, chosenFilter, photo) {
+//функция, которая используется, чтобы скрыть одну форму и показать другую
+function showAndHide (whatToBeShown, whatToBeHidden) {
+  whatToBeShown.classList.remove('invisible');
+  whatToBeHidden.classList.add('invisible');
+}
+
+//функция, чтобы получить значение стиля из input'а
+function getFilterClass (htmlNode) {
+  return 'filter-' + htmlNode.value;
+}
+
+//функция, которая получает список стилей, чтобы далее снять их у фото, прежде чем добавлять очередной
+function getListOfFilters (filters) {
+  var filtersArr = [];
+  for (var i = 0; i < filters.length; i++) {
+    filtersArr.push(getFilterClass(filters[i]));
+  }
+  return filtersArr;
+}
+
+//функция, которая непосредственно переключает фильтр
+function changeFilter (filterName, filterList, mainPhoto) {
   for (var i = 0; i < filterList.length; i++) {
-    if (photo.classList.contains(filterList[i]) + "") {
-      photo.classList.remove(filterList[i]) + "";
+    if (mainPhoto.classList.contains(filterList[i])) {
+      mainPhoto.classList.remove(filterList[i]);
     }
+    mainPhoto.classList.add(filterName);
   }
-  photo.classList.add(chosenFilter);
 }
 
-function changePhotoScale(input, photo) {
-  photo.cssText = 'transform: scale(' + toNumber(input.value) / 100 + ')';
-}
-
-function createClassForPreview (id) {
-  return id.substring(7);
-}
-
-function createClassList (arrOfInputs) {
-  var classArr = [];
-  for (var i = 0; i < arrOfInputs.length; i++) {
-    classArr.push(createClassForPreview(arrOfInputs[i].id))
-  }
-  return classArr;
-}
-
-overlayForm.filters.field.addEventListener('click', function(evt) {
-  if (evt.target.tagName == 'INPUT') {
-    changeFilter(createClassList(this.querySelectorAll('input')), createClassForPreview(evt.target.id), overlayForm.mainPhoto())
-  }
-})
-
-uploadForm.uploadfile().addEventListener('change', function() {
-  if (!(uploadForm.htmlNode.classList.contains('invisible')) && (overlayForm.htmlNode.classList.contains('invisible'))) {
-    changeViewedForm(uploadForm.htmlNode, overlayForm.htmlNode)
-  }
-})
-overlayForm.cancelBtn().addEventListener('click', function() {
-
-  if ((uploadForm.htmlNode.classList.contains('invisible')) && (!(overlayForm.htmlNode.classList.contains('invisible')))) {
-    uploadForm.uploadfile().value = ""; /*
-      Была проблема:
-      1) Выбираем файл
-      2) Закрываем поле кадрирования
-      3) Выбираем тот же самый файл.
-      И тут поле кадрирования не открывалось. Чтобы оно открылось снова, я добавил эту строчку
-    */
-    changeViewedForm(uploadForm.htmlNode, overlayForm.htmlNode)
-  }
+uploadPhotoInput.addEventListener('change', function() {
+  showAndHide(uploadOverlay, uploadForm)
 });
 
-overlayForm.resize.htmlNode.addEventListener('click', function(evt) {
-  if (evt.target.tagName != 'BUTTON') return;
-  var opsType;
-  if (evt.target.classList.contains('upload-resize-controls-button-dec')) opsType = '-';
-  if (evt.target.classList.contains('upload-resize-controls-button-inc')) opsType = '+';
-  changeInputValue(overlayForm.resize, opsType);
-  changePhotoScale(overlayForm.resize.input, overlayForm.mainPhoto().querySelector('img'));
+uploadFormCancel.addEventListener('click', function() {
+  uploadPhotoInput.value = "";
+  showAndHide(uploadForm, uploadOverlay);
 });
+
+for (var i = 0; i < filters.length; i++) {
+  filters[i].addEventListener('click', function(evt) {
+    changeFilter(getFilterClass(evt.target), getListOfFilters(filters), photoPreview)
+  })
+}
+
+for (var i = 0; i < scaleField.buttons().length; i++) {
+  scaleField.buttons()[i].addEventListener('click', function(evt) {
+    var decOrInc = evt.target.classList.contains('upload-resize-controls-button-dec') ? 'dec' : 'inc';
+    changeScaleValue(scaleField.valueWindow(), decOrInc, scaleField.restrictions.min, scaleField.restrictions.max, scaleField.restrictions.step);
+    changeScale(photoPreview, scaleField.valueWindow().value);
+  })
+}
