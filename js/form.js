190cbc576441a48239b4bf487473aeeccf60df54
Californium251 <californium251@gmail.com>
2017-02-13 21:43:58 +0300

    Инкапсулировал form.js

--- js/form.js
+++ js/form.js
@@ -3,27 +3,30 @@
  */
 'use strict';
 
-var uploadForm = document.querySelector('.upload-form');
-var uploadPhotoInput = document.getElementById('upload-file');
-var uploadOverlay = document.querySelector('.upload-overlay');
-var uploadFormCancel = uploadOverlay.querySelector('.upload-form-cancel');
-var allFilters = document.querySelector('.upload-filter-controls');
-var photoPreview = document.querySelector('img.filter-image-preview');
-var scaleField = document.querySelector('fieldset.upload-resize-controls');
+window.form = (function () {
+  var uploadForm = document.querySelector('.upload-form');
+  var uploadPhotoInput = document.getElementById('upload-file');
+  var uploadOverlay = document.querySelector('.upload-overlay');
+  var uploadFormCancel = uploadOverlay.querySelector('.upload-form-cancel');
+  var allFilters = document.querySelector('.upload-filter-controls');
+  var photoPreview = document.querySelector('img.filter-image-preview');
+  var scaleField = document.querySelector('fieldset.upload-resize-controls');
 
-function showAndHide(whatToBeShown, whatToBeHidden) {
-  whatToBeShown.classList.remove('invisible');
-  whatToBeHidden.classList.add('invisible');
-}
+  function showAndHide(whatToBeShown, whatToBeHidden) {
+    whatToBeShown.classList.remove('invisible');
+    whatToBeHidden.classList.add('invisible');
+  }
 
-uploadPhotoInput.addEventListener('change', function () {
-  showAndHide(uploadOverlay, uploadForm);
-});
+  uploadPhotoInput.addEventListener('change', function () {
+    showAndHide(uploadOverlay, uploadForm);
+  });
 
-uploadFormCancel.addEventListener('click', function () {
-  uploadPhotoInput.value = '';
-  showAndHide(uploadForm, uploadOverlay);
-});
+  uploadFormCancel.addEventListener('click', function () {
+    uploadPhotoInput.value = '';
+    showAndHide(uploadForm, uploadOverlay);
+  });
 
-window.initializeFlters(allFilters, photoPreview);
-window.createScale(scaleField, photoPreview);
+  window.initializeFlters(allFilters, photoPreview);
+  window.createScale(scaleField, photoPreview);
+})();
+
